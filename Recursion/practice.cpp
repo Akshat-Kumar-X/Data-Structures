924daf0f517957f9e3a22e1d8e2a5688830dbf81
Akshat Kumar <akshatkumar10203@gmail.com>
2024-10-28 01:13:14 +0530

    new changes

---
+++ Recursion/practice.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int foo (int num, int n) {
+    int y = foo(num, n/2);
+    if (n%2 == 0) {
+        return 
+    }
+}
+
+int main() {
+    int n = 5;
+    cout << foo(5,3) << endl;
+}
+ 
