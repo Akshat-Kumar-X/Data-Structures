dc33f5d9cc53e285fd76c211cee5c77e22e3d38d
Akshat Kumar <akshatkumar10203@gmail.com>
2024-10-31 10:26:28 +0530

    Minor Chages

--- Recursion/All Combinations/Word break.cpp
+++ Recursion/All Combinations/Word break.cpp
@@ -24,3 +24,36 @@ public:
         return solve(0, s, mp);
     }
 };
+
+// With Memoization
+
+class Solution {
+public:
+    bool solve(int index, string s, unordered_map<string, int>& mp, vector<int>& memo) {
+        if (index == s.length()) {
+            return true;
+        }
+        
+        if (memo[index] != -1) return memo[index];
+        
+        string subStr;
+        for (int i = index; i < s.length(); i++) {
+            subStr += s[i];
+            if (mp.find(subStr) != mp.end()) {
+                if (solve(i + 1, s, mp, memo)) {
+                    return memo[index] = true;
+                }
+            }
+        }
+        return memo[index] = false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string, int> mp;
+        for (const auto& word : wordDict) {
+            mp[word] = 1;
+        }
+        vector<int> memo(s.length(), -1);
+        return solve(0, s, mp, memo);
+    }
+};
