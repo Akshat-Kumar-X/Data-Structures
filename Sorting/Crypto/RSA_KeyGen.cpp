f8bf83b4fd063a4f7c62cb212b56c26eba7db63a
Akshat Kumar <akshatkumar10203@gmail.com>
2025-04-06 15:47:35 +0530

    new changes

--- Sorting/Crypto/RSA_KeyGen.cpp
+++ Sorting/Crypto/RSA_KeyGen.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 
-int modInverse(int a, int b) {
-    int t1 = 0, t2 = 1;
-    int r1 = b, r2 = a;
+ll modExpo(ll base, ll exponent, ll modulo) {
+    ll result = 1;
+    base = base % modulo;
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            result = (result * base) % modulo;
+
+        base = (base * base) % modulo;
+        exponent = exponent / 2;
+    }
+    return result;
+}
+
+ll modInverse(ll a, ll b) {
+    ll t1 = 0, t2 = 1;
+    ll r1 = b, r2 = a;
 
     while (r2 > 0) {
-        int q = r1 / r2;
-        int r = r1 - q * r2;
+        ll q = r1 / r2;
+        ll r = r1 - q * r2;
         r1 = r2;
         r2 = r;
 
-        int t = t1 - q * t2;
+        ll t = t1 - q * t2;
         t1 = t2;
         t2 = t;
     }
@@ -24,22 +39,32 @@ int modInverse(int a, int b) {
 }
 
 int main() {
-    int p, q, e;
+    ll p, q, e;
     cin >> p >> q >> e;
 
-    int n = p * q;
-    int phi = (p - 1) * (q - 1);
+    ll plainText;
+    cin >> plainText;
 
-    // Check if e and phi are coprime
+    ll n = p * q;
+    ll phi = (p - 1) * (q - 1);
+
     if (__gcd(e, phi) != 1) {
         cout << "Invalid e. It must be coprime with phi(n)." << endl;
         return 1;
     }
 
-    int d = modInverse(e, phi);
+    ll d = modInverse(e, phi);
 
     cout << "Public Key: (" << e << ", " << n << ")" << endl;
     cout << "Private Key: (" << d << ", " << n << ")" << endl;
+
+    // Encryption:
+    ll cipherText = modExpo(plainText, e, n);
+    cout << "Encrypted (Cipher) Text: " << cipherText << endl;
+
+    // Decryption:
+    ll decryptedText = modExpo(cipherText, d, n);
+    cout << "Decrypted Plaintext: " << decryptedText << endl;
 
     return 0;
 }
